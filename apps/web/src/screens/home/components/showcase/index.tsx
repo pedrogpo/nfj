e7f0ff8c6ffb83232fb6c867725a3d8cf0859aa5
Plowdzn <50748006+Plowdzn@users.noreply.github.com>
2022-11-04 05:01:43 -0300

    fixed swipe ref

--- apps/web/src/screens/home/components/showcase/index.tsx
+++ apps/web/src/screens/home/components/showcase/index.tsx
@@ -10,54 +10,64 @@ import { EffectCoverflow, Pagination, Navigation } from 'swiper'
 import { BiChevronLeft, BiChevronRight } from 'react-icons/bi'
 import { useCallback, useRef } from 'react'
 
+import SwiperCore from "swiper"
+
 export default function Jumbotron() {
-  const sliderRef = useRef(null)
+  const sliderRef = useRef<SwiperCore>()
 
   const handlePrev = useCallback(() => {
     if (!sliderRef.current) return
-    sliderRef.current.swiper.slidePrev()
+    sliderRef.current.slidePrev()
   }, [])
 
   const handleNext = useCallback(() => {
     if (!sliderRef.current) return
-    sliderRef.current.swiper.slideNext()
+    sliderRef.current.slideNext()
   }, [])
 
   return (
     <S.Showcase>
-      <Swiper
-        ref={sliderRef}
-        effect={'coverflow'}
-        loop={true}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView="auto"
-        spaceBetween={120}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 300,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        pagination={true}
-        navigation={true}
-        modules={[EffectCoverflow, Pagination, Navigation]}
+      <div>
+        <Swiper
+          onInit={(core) => {sliderRef.current = core}}
+          effect={'coverflow'}
+          loop={true}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView="auto"
+          spaceBetween={120}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 300,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={true}
+          navigation={true}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+        >
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div
+        className="prev-arrow pagination__left pagination__icon"
+        onClick={handlePrev}
       >
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-      </Swiper>
-      <div className="prev-arrow pagination__left pagination__icon" onClick={handlePrev}>
         <BiChevronLeft size={30} />
       </div>
-      <div className="next-arrow pagination__right pagination__icon" onClick={handleNext}>
+      <div
+        className="next-arrow pagination__right pagination__icon"
+        onClick={handleNext}
+      >
         <BiChevronRight size={30} />
       </div>
     </S.Showcase>
