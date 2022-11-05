d60b806cee34a60fbfa69b3c02de43b05cc35b4f
Plowdzn <50748006+Plowdzn@users.noreply.github.com>
2022-11-05 01:17:02 -0300

    footer

---
+++ apps/web/src/components/molecules/footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Footer = styled.footer`
+  color: white;
+
+  padding: 40px 0;
+
+  ul {
+    margin-top: 0.5rem;
+    list-style-type: none;
+    padding-left: 0;
+    li {
+      line-height: 35px;
+    }
+  }
+`
--- apps/web/src/components/molecules/index.ts
+++ apps/web/src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export { default as Navbar } from "../molecules/navbar";
+export { default as Footer } from "../molecules/footer";
 export { default as ProductCard } from "../molecules/product-card";
